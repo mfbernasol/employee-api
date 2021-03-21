e264bf6880c0dbf92b0d5f1e23cdd6085b0070f8
mfbernasol <mfbernasol@gmail.com>
2021-03-21 18:52:09 -0500

    inital commit create API

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+app.use(express.urlencoded({extended:true}));
+
+mongoose.connect('mongodb+srv://test-user:user1234@cluster0.jdj0v.mongodb.net/employee', { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+
+const employeeSchema = {
+    lastName: String,
+    firstName: String, 
+    email: String, 
+    workID: String,
+    datePositiveTest: {
+      type: Date
+    },
+    dateNegativeTest: 
+    {
+      type: Date
+    },
+    vacStatus: {
+      type: Boolean,
+      default: false
+    },
+    infected: {
+      type: Boolean,
+      default: false
+    }, 
+    dateOfVac: {
+      type: Date
+    },
+    inQuarantine: {
+      type: Boolean,
+      default: false
+    },
+    quarDate: {
+      type: Date
+    },
+    quarEndDate: {
+      type: Date
+    }
+    
+  }
+
+const Employee = mongoose.model("Employee", employeeSchema);
+
+ app.get("/",function(req,res) {
+    Employee.find(function(err,foundEmployees){
+        if(!err){
+            res.send(foundEmployees);
+        }else{
+            res.send(err);
+        }
+       
+    });
+ });
+
+
+  
+const port = process.env.PORT || 4000
+// const port2 = 5300
+app.listen(port, () => console.log(
+  `Express started on http://localhost:${port}; ` +
+  `press Ctrl-C to terminate.`));
