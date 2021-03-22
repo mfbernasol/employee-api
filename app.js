9420a4606ba047b37bf3852dbfe30949af3d4839
mfbernasol <mfbernasol@gmail.com>
2021-03-21 19:59:16 -0500

    fixing

--- app.js
+++ app.js
@@ -5,48 +5,48 @@ const mongoose = require('mongoose');
 
 app.use(express.urlencoded({extended:true}));
 
-mongoose.connect('mongodb+srv://test-user:user1234@cluster0.jdj0v.mongodb.net/employee', { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+// mongoose.connect('mongodb+srv://test-user:user1234@cluster0.jdj0v.mongodb.net/employee', { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
 
-const employeeSchema = {
-    lastName: String,
-    firstName: String, 
-    email: String, 
-    workID: String,
-    datePositiveTest: {
-      type: Date
-    },
-    dateNegativeTest: 
-    {
-      type: Date
-    },
-    vacStatus: {
-      type: Boolean,
-      default: false
-    },
-    infected: {
-      type: Boolean,
-      default: false
-    }, 
-    dateOfVac: {
-      type: Date
-    },
-    inQuarantine: {
-      type: Boolean,
-      default: false
-    },
-    quarDate: {
-      type: Date
-    },
-    quarEndDate: {
-      type: Date
-    }
+// const employeeSchema = {
+//     lastName: String,
+//     firstName: String, 
+//     email: String, 
+//     workID: String,
+//     datePositiveTest: {
+//       type: Date
+//     },
+//     dateNegativeTest: 
+//     {
+//       type: Date
+//     },
+//     vacStatus: {
+//       type: Boolean,
+//       default: false
+//     },
+//     infected: {
+//       type: Boolean,
+//       default: false
+//     }, 
+//     dateOfVac: {
+//       type: Date
+//     },
+//     inQuarantine: {
+//       type: Boolean,
+//       default: false
+//     },
+//     quarDate: {
+//       type: Date
+//     },
+//     quarEndDate: {
+//       type: Date
+//     }
     
-  }
+//   }
 
-const Employee = mongoose.model("Employee", employeeSchema);
-app.get("/",function(req,res){
-  res.send("Hello World");
-});
+// const Employee = mongoose.model("Employee", employeeSchema);
+// app.get("/",function(req,res){
+//   res.send("Hello World");
+// });
 
 //  app.get("/employees",function(req,res) {
 //     Employee.find(function(err,foundEmployees){
